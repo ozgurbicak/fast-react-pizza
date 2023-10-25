049424ba7b345f7e5fb2cc5d5fe8347beb93bc83
Ozgur Bicak <ozgurbicak62@gmail.com>
2023-10-25 21:32:23 +0300

    l

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200">
-      <p className="text-stone-300 font-semibold">
+    <div className="bg-stone-800 text-stone-200 uppercase p-4 sm:px-6 text-sm md:text-base flex items-center justify-between">
+      <p className="text-stone-300 font-semibold space-x-4 sm:spaxe-x-6">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
